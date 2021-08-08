0241c1d963b5dac12f989eaeb90110b0bc9847ee
ManuDev03 <manuprajan03@gmail.com>
2021-08-08 11:11:11 +0530

    property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,6 +9,13 @@ import { Component } from '@angular/core'
 export class ServerComponent{
     serverId:number = 10;
     serverStatus:string = 'offline';
+    allowNewServer = false
+
+    constructor(){
+        setTimeout(()=>{
+            this.allowNewServer = true
+        },2000)
+    }
 
     getServerStatus(){
       return this.serverStatus  
