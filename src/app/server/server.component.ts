68ec8ec7463b92c4c4acd11f554f942db979c15d
ManuDev03 <manuprajan03@gmail.com>
2021-08-09 11:43:43 +0530

    Applying css classes dynamically with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
-    styleUrls: ['./server.component.css']
+    // styleUrls: ['./server.component.css']
+    styles:[`
+    .online {
+        color: white;
+    }`]
 })
 
 export class ServerComponent{
